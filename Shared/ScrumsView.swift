901abc536d1a20d1622636e32d88d7685168e952
Bnowako <bnowakowski98@gmail.com>
2022-04-27 21:28:22 +0200

    add cards list view

---
+++ Shared/ScrumsView.swift
@@ -0,0 +1,28 @@
+//
+//  ScrumsView.swift
+//  scrumdinger (iOS)
+//
+//  Created by Błażej Nowakowski on 27/04/2022.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
