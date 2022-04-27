d2ea01b972f01f5f060293f52a06e81a076ec76a
Bnowako <bnowakowski98@gmail.com>
2022-04-27 20:22:20 +0200

    Initial Commit

---
+++ Shared/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  Shared
+//
+//  Created by Błażej Nowakowski on 27/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
